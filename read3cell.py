19220730c02c42525b8de817f904315b3a5efff6
Sl1m621 <paningt56@gmail.com>
2023-03-29 17:48:27 +0300

    fix

--- read3cell.py
+++ read3cell.py
@@ -15,10 +15,4 @@ def readalldata():
         #     print(r['data'][i]['params'][0]['value'])
 
         return r['data']
-
-<<<<<<< HEAD
-=======
-
-
->>>>>>> be955e458cd5bc3b5d13a2627756e09a1fabf048
 readalldata()
